b69193bc28620f1395e4c964a21d32b5d10c9c91
maxabarcap <maximilianoabarcap@gmail.com>
2021-12-13 14:24:16 -0300

    added operate function

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const add = function(a,b) {
-    return a+b;
-}
+    return a+b;}
 
 const substract = function (a,b) {
-    return a-b;
-}
+    return a-b;}
 
 const multiply = function(a,b) {
-    return a*b;
-}
+    return a*b;}
 
 const divide = function(a,b) {
-    return a/b;
-}
+    return a/b;}
+
+const operate = function(operator,a,b) {
+    if (operator == "+") {
+        return add(a,b)}
+    else if (operator == "-") {
+        return substract(a,b)}
+    else if (operator == "*") {
+        return multiply(a,b)}
+    else if (operator == "/") 
+        {return divide(a,b)}    }
+
