849eef49e3999e6eb9714debf584636953c7e535
maxabarcap <maximilianoabarcap@gmail.com>
2021-12-14 21:21:42 -0300

    added button funcitonality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const add = function(a,b) {
-    return a+b;}
+    return parseInt(a)+parseInt(b);}
 
 const substract = function (a,b) {
     return a-b;}
@@ -8,7 +8,10 @@ const multiply = function(a,b) {
     return a*b;}
 
 const divide = function(a,b) {
-    return a/b;}
+    if (b==0) {
+        return "nice try"}
+    else {
+        return a/b;}}
 
 const operate = function(operator,a,b) {
     if (operator == "+") {
@@ -20,18 +23,78 @@ const operate = function(operator,a,b) {
     else if (operator == "/") 
         {return divide(a,b)}    }
 
-let displayValue = "";
+let lastResult = "0";
+let savedOperand = "";
+let operandOne = "";
+let isLastKeyOp = false;
+let isLastKeyEq = false;
+
+let displayValue = "0";
 let displayBox = document.querySelector(".display");
 displayBox.innerHTML = displayValue;
 
 const buttons = document.querySelectorAll(".btn")
 buttons.forEach(button => button.addEventListener('click', function() {
-    displayValue += button.innerHTML;
-    displayBox.innerHTML = displayValue
-    console.log(displayValue)} ) )
+    if (isLastKeyOp == false) {
+        if (displayValue == 0) {
+            displayValue = button.innerHTML;
+            displayBox.innerHTML = displayValue
+            console.log(displayValue)
+        }
+        else if (displayValue == 'nice try') {
+            displayValue = button.innerHTML;
+            displayBox.innerHTML = displayValue
+            console.log(displayValue)
+        }
+        else {
+            displayValue += button.innerHTML;
+            displayBox.innerHTML = displayValue
+            console.log(displayValue)}
+        }
+    else {
+        displayValue = button.innerHTML;
+        displayBox.innerHTML = displayValue;
+        isLastKeyOp = false
+        console.log(displayValue)
+    }
+    isLastKeyEq = false
+} ) )
+
+const operator = document.querySelectorAll(".ope") 
+operator.forEach(button => button.addEventListener('click', function() {
+    if (operandOne == "") {
+        savedOperand = button.innerHTML;
+        operandOne = displayValue;
+        isLastKeyOp = true;
+
+    }
+    else {
+        calculate()
+        savedOperand = button.innerHTML
+        operandOne = lastResult
+        isLastKeyOp = true
+    }
+    isLastKeyEq = false;
+} ) )
 
 const clear = document.querySelectorAll(".clear")
 clear.forEach(button => button.addEventListener('click', function() {
-    displayValue = "";
+    displayValue = "0";
+    lastResult = "0";
+    savedOperand = "";
+    operandOne = "";
+    isLastKeyOp = false;
     displayBox.innerHTML = displayValue} ) )  
 
+const calculate = function () {
+   lastResult = operate(savedOperand,operandOne,displayValue)
+   displayValue = lastResult
+   displayBox.innerHTML = displayValue
+}
+
+const equal = document.querySelectorAll(".equal")
+equal.forEach(button => button.addEventListener('click', function() {
+    if (isLastKeyEq == false) {
+        calculate()
+    }
+} ) )
